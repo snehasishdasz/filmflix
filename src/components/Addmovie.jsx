dac2681a34cb526c6df213d73a009cb6615ca69a
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-20 03:29:22 +0530

    Firebase/add Loader

--- src/components/Addmovie.jsx
+++ src/components/Addmovie.jsx
@@ -1,61 +1,125 @@
-import React, { useState } from 'react'
-import {TailSpin} from "react-loader-spinner";
+import React, { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import {addDoc} from "firebase/firestore";
+import { moviesRef } from "../firebase/Firebase";
+import swal from "sweetalert"
 
 const Addmovie = () => {
-    const[form,setForm] = useState({
+    const [form, setForm] = useState({
         title: "",
         year: "",
         description: "",
-        image: ""
+        image: "",
     });
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const adMovie = async () => {
+        setLoading(true);
+        try{
+            await addDoc(moviesRef,form);
+            swal({
+                title: "Successfully Added",
+                icon: "success",
+                buttons: false,
+                timer: 3000
+            })
+        }catch(err){
+            swal({
+                title: "err",
+                icon: "error",
+                buttons: false,
+                timer: 3000
+            })
+        }
+        setLoading(false);
+    }
+
     return (
         <div>
             <section class="text-white body-font relative">
                 <div class="container px-5 py-8 mx-auto">
                     <div class="flex flex-col text-center w-full mb-4">
-                        <h1 class="sm:text-3xl text-xl font-medium title-font mb-4 text-white">Add New Movie</h1>
+                        <h1 class="sm:text-3xl text-xl font-medium title-font mb-4 text-white">
+                            Add New Movie
+                        </h1>
                     </div>
                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
                         <div class="flex flex-wrap -m-2">
                             <div class="p-2 w-1/2">
                                 <div class="relative">
-                                    <label for="name" class="leading-7 text-sm text-white">Title</label>
-                                    <input type="text" id="name" name="name" value={form.title} 
-                                    onChange={(e)=>setForm({...form,title:e.target.value})}
-                                    class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <label for="name" class="leading-7 text-sm text-white">
+                                        Title
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        value={form.title}
+                                        onChange={(e) =>
+                                            setForm({ ...form, title: e.target.value })
+                                        }
+                                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    />
                                 </div>
                             </div>
                             <div class="p-2 w-1/2">
                                 <div class="relative">
-                                    <label for="email" class="leading-7 text-sm text-white">Year</label>
-                                    <input type="email" id="email" name="email" value={form.year} 
-                                    onChange={(e)=>setForm({...form,year:e.target.value})} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <label for="email" class="leading-7 text-sm text-white">
+                                        Year
+                                    </label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        value={form.year}
+                                        onChange={(e) => setForm({ ...form, year: e.target.value })}
+                                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    />
                                 </div>
                             </div>
                             <div class="p-2 w-full">
                                 <div class="relative">
-                                    <label for="message" class="leading-7 text-sm text-white">Image link</label>
-                                    <input id="message" name="message" value={form.image} 
-                                    onChange={(e)=>setForm({...form,image:e.target.value})} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <label for="message" class="leading-7 text-sm text-white">
+                                        Image link
+                                    </label>
+                                    <input
+                                        id="message"
+                                        name="message"
+                                        value={form.image}
+                                        onChange={(e) =>
+                                            setForm({ ...form, image: e.target.value })
+                                        }
+                                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    />
                                 </div>
                             </div>
                             <div class="p-2 w-full">
                                 <div class="relative">
-                                    <label for="message" class="leading-7 text-sm text-white">Description</label>
-                                    <textarea id="message" name="message" value={form.description} 
-                                    onChange={(e)=>setForm({...form,description:e.target.value})} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    <label for="message" class="leading-7 text-sm text-white">
+                                        Description
+                                    </label>
+                                    <textarea
+                                        id="message"
+                                        name="message"
+                                        value={form.description}
+                                        onChange={(e) =>
+                                            setForm({ ...form, description: e.target.value })
+                                        }
+                                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                    ></textarea>
                                 </div>
                             </div>
                             <div class="p-2 w-full">
-                                <button class="flex mx-auto text-white bg-transparent border-2 border-red-500 py-2 px-8 focus:outline-none hover:bg-red-500   rounded  text-lg">{loading ? <TailSpin height={25} color="white" /> : 'Submit'}</button>
+                                <button onClick={adMovie} class="flex mx-auto text-white bg-transparent border-2 border-red-500 py-2 px-8 focus:outline-none hover:bg-red-500   rounded  text-lg">
+                                    {loading ? <TailSpin height={25} color="white" /> : "Submit"}
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
         </div>
-    )
-}
+    );
+};
 
-export default Addmovie
+export default Addmovie;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,69 +1,43 @@
-import React, { useState } from 'react'
+import { getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { Audio,Dna,Grid } from 'react-loader-spinner';
 import ReactStars from 'react-stars';
+import { getDoc } from 'firebase/firestore';
+import { moviesRef } from '../firebase/Firebase';
+
+
 const Cards = () => {
-    const[data,setData]=useState([
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 5,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 4,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 3,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 2,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 1,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 3.5,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 1.5,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-        {
-            name: "Avenger Endgame",
-            year: "2018",
-            rating: 5,
-            img: "https://rukminim2.flixcart.com/image/850/1000/k2gh30w0/poster/h/n/8/medium-avengers-endgame-hd-poster-wall-decor-size-12x18-inch-original-imafht6wyfzfjzcb.jpeg?q=90"
-        },
-    ]);
+    const[data,setData]=useState([]);
+    const [loading, setLoading] = useState(false);
+
+     useEffect(() =>{
+        async function getData(){
+            setLoading(true);
+            const _data = await getDocs(moviesRef);
+            _data.forEach((doc)=>{
+                setData((prv)=>[...prv,doc.data()])
+            })
+            setLoading(false);
+        }
+        getData();
+    },[])
+
     return (
         <div className='flex flex-wrap justify-between p-3 mt-2 '>
-        {data.map((e,i) => {
+        {loading ?<div className="w-full flex justify-center items-center h-96 "><Grid color="#8B5CF6"  height={80} width="150"  /></div> :
+        
+        data.map((e,i) => {
             return(
                 <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointe  mt-6 transition-all duration-500'>
-                <img className="h-72" src={e.img} alt="" />
-                <h1><span className='text-gray-500'>Name:</span> {e.name}</h1>
+                <img className="h-72" src={e.image} alt="" />
+                <h1><span className='text-gray-500'>Name:</span> {e.title}</h1>
                 <h1 className='flex items-center'><span className='text-gray-500 mr-1'>Rating:</span> 
-                <ReactStars size={20} half={true} value={e.rating} edit={false} />
+                <ReactStars size={20} half={true} value={5} edit={false} />
                 </h1>
                 <h1><span className='text-gray-500'>Year:</span> {e.year}</h1>
             </div>
-            )})}
+            )})
+        }
             
         </div>
     )
